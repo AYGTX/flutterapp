b1047f4819923b727278af70649aaaecd9aae663
Younes Regaieg <y.regaieg@gmail.com>
2020-11-07 22:17:15 +0100

    Fix assets loading

--- lib/question.dart
+++ lib/question.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
   final String qText;
-Question(this.qText);
+
+  Question(this.qText);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       margin: const EdgeInsets.all(15.0),
-      child: Image(
-image: AssetImage('images/'+qText)
-      ),
-  );
-  } 
+      child: Image.asset('assets/images/' + qText),
+    );
+  }
 }
